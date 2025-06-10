af997c4bb0f70a7059e55c72e6a7d2464dc8e020
kkitis <kumarkapil00649@gmail.com>
2025-06-10 13:42:47 +0530

    learning file io

---
+++ explorer/learn.py
@@ -0,0 +1,6 @@
+f = open('C:\pythonprojects\pythonprojects\explorer\demo.txt', 'r')
+data= f.read()
+print(data)
+print(type(data))
+f.close()
+# The code reads the contents of 'demo.txt' and prints it along with its type.
