f20967f2168ee0a08d67cb95200a77656b3ba507
kkitis <kumarkapil00649@gmail.com>
2025-06-20 15:48:21 +0530

    added attempt feature

--- explorer/numberguess.py
+++ explorer/numberguess.py
@@ -4,10 +4,15 @@ holy_number = random.randint(1,100)
 
 while True:
     guess = int(input("Guess the holy number (between 1 and 100): "))
+    count = 0
     if guess < holy_number:
         print("Too low! Try again.")
+        count += 1
     elif guess > holy_number:
         print("too high! Try again.")
+        count += 1
     else:
         print("thats correct ma ma oooooooooooo!")
-        break 
+        break
+
+print(f"Total attempts: {count}")
