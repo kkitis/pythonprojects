317d11ef737640c0473bdbed029d46787651da37
kkitis <kumarkapil00649@gmail.com>
2025-06-21 11:47:53 +0530

    modified count

--- explorer/numberguess.py
+++ explorer/numberguess.py
@@ -1,18 +1,18 @@
 import random 
 
 holy_number = random.randint(1,100)
+count = 0
 
 while True:
     guess = int(input("Guess the holy number (between 1 and 100): "))
-    count = 0
+    count+=1
+    
     if guess < holy_number:
         print("Too low! Try again.")
-        count += 1
     elif guess > holy_number:
         print("too high! Try again.")
-        count += 1
     else:
         print("thats correct ma ma oooooooooooo!")
+        print(f"Total attempts: {count}")
+        
         break
-
-print(f"Total attempts: {count}")
